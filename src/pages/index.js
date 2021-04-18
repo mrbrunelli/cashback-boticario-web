a0991a1c62b9348eeee8c47fdf603a125608ff4c
Matheus R. Brunelli <matheus.brunelli@gmail.com>
2021-04-18 20:34:00 -0300

    feat: add login page

---
+++ src/pages/index.js
@@ -0,0 +1,46 @@
+import Head from "next/head";
+import styles from "../styles/pages/Login.module.css";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Login - Boticário</title>
+        <link rel="shortcut icon" href="/boticario.png" type="image/x-icon" />
+      </Head>
+      <div className={styles.container}>
+        <section className={styles.hello}>
+          <h1>O Boticário</h1>
+          <h4>Frete GRÁTIS em todas as compras</h4>
+        </section>
+        <section className={styles.login}>
+          <div className={styles.card}>
+            <header>
+              <h1>Faça seu Login</h1>
+            </header>
+            <form>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                placeholder="Digite seu E-mail"
+                required
+              />
+              <input
+                type="password"
+                name="password"
+                id="password"
+                placeholder="Digite sua Senha"
+                required
+              />
+              <button type="submit">
+                Entrar
+              </button>
+              <small>Não tem uma conta? <a href="">Cadastre-se aqui!</a></small>
+            </form>
+          </div>
+        </section>
+      </div>
+    </>
+  );
+}
