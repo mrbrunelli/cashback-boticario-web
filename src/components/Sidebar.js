ae6358e0a0b92dd7e2857c2f9e7100e84e0b1cdf
Matheus R. Brunelli <matheus.brunelli@gmail.com>
2021-04-21 15:23:53 -0300

    feat: add sidebar component

---
+++ src/components/Sidebar.js
@@ -0,0 +1,62 @@
+import { useContext } from "react";
+import { useRouter } from "next/router";
+import SideNav, { NavIcon, NavItem, NavText } from "@trendmicro/react-sidenav";
+import "@trendmicro/react-sidenav/dist/react-sidenav.css";
+import { FiHome, FiPlus, FiShoppingCart, FiLogOut } from "react-icons/fi";
+import { SidebarContext } from "../contexts/Sidebar";
+import { useCookies } from "react-cookie";
+import styles from "../styles/components/Sidebar.module.css";
+
+export default function Sidebar() {
+  const router = useRouter();
+  const [cookie, setCookie, removeCookie] = useCookies(["dealer"]);
+  const { currentPage, render } = useContext(SidebarContext);
+
+  const logOut = () => {
+    removeCookie("dealer");
+    router.push("/");
+  }
+
+  return (
+    <SideNav
+      onSelect={(selected) => render(selected)}
+      className={styles.color}
+    >
+      <SideNav.Toggle />
+      <SideNav.Nav defaultSelected={currentPage}>
+        <NavItem eventKey="home">
+          <NavIcon>
+            <FiHome size="1rem" />
+          </NavIcon>
+          <NavText>
+            Home
+          </NavText>
+        </NavItem>
+        <NavItem eventKey="add">
+          <NavIcon>
+            <FiPlus />
+          </NavIcon>
+          <NavText>
+            Cadastrar Compra
+          </NavText>
+        </NavItem>
+        <NavItem eventKey="report">
+          <NavIcon>
+            <FiShoppingCart />
+          </NavIcon>
+          <NavText>
+            Relatório de Compras
+          </NavText>
+        </NavItem>
+        <NavItem onClick={logOut}>
+          <NavIcon>
+            <FiLogOut />
+          </NavIcon>
+          <NavText>
+            Sair
+          </NavText>
+        </NavItem>
+      </SideNav.Nav>
+    </SideNav>
+  );
+}
