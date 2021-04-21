9bcdc06474ae83dc889944c9099c93d89a4964e8
Matheus R. Brunelli <matheus.brunelli@gmail.com>
2021-04-21 18:40:56 -0300

    feat: add cashback helpers

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -22,3 +22,22 @@ export const formatBRL = (money) => {
   return Intl.NumberFormat("pt-BR", { style: "currency", currency: "BRL" })
     .format(money);
 };
+
+/**
+ * @param {number} bigger
+ * @param {number} smaller
+ */
+export const calculateDiscountPercent = (bigger, smaller) => {
+  const percent = ((bigger - smaller) / bigger) * 100;
+  return percent.toFixed(2);
+};
+
+/**
+ * Example: 3% of 25 = 24.25 = R$ 0.75 of CashBack
+ * @param {number} amount - Amount value to calculate - Ex: 25
+ * @param {number} percent - Percentage of CashBack - Ex: 3
+ */
+ export const calculateCashBack = (amount, percent) => {
+  const percentFormated = 1 - (percent / 100);
+  return amount - (amount * percentFormated);
+};
