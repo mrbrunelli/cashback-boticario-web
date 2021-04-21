762a377ee977d6cf8f1000c65a1f1c66f10ccdd8
Matheus R. Brunelli <matheus.brunelli@gmail.com>
2021-04-21 15:25:26 -0300

    feat: implement cookie in login

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,30 @@
 import Head from "next/head";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { useCookies } from "react-cookie";
 import styles from "../styles/pages/Login.module.css";
+import { useForm } from "react-hook-form";
+import api from "../services/api";
 
 export default function Login() {
+  const [cookie, setCookie] = useCookies(["dealer"]);
+  const { register, handleSubmit } = useForm();
+  const router = useRouter();
+
+  const onSubmit = async (data) => {
+    try {
+      const res = await api.post("/login", data);
+      setCookie("dealer", JSON.stringify(res.data), {
+        path: "/",
+        maxAge: 3600 * 8,
+        sameSite: true,
+      });
+      router.push("/home");
+    } catch (e) {
+      alert("Erro ao realizar Login.");
+    }
+  };
+
   return (
     <>
       <Head>
@@ -19,19 +41,20 @@ export default function Login() {
             <header>
               <h1>Faça seu Login</h1>
             </header>
-            <form>
+            <form onSubmit={handleSubmit(onSubmit)}>
               <input
+                {...register("email")}
                 type="email"
-                name="email"
                 id="email"
                 placeholder="Digite seu E-mail"
                 required
               />
               <input
+                {...register("password")}
                 type="password"
-                name="password"
                 id="password"
                 placeholder="Digite sua Senha"
+                maxLength="20"
                 required
               />
               <button type="submit">
@@ -39,7 +62,9 @@ export default function Login() {
               </button>
               <small>
                 Não tem uma conta?
-                <Link href="/register"><a> Cadastre-se aqui!</a></Link>
+                <Link href="/register">
+                  <a> Cadastre-se aqui!</a>
+                </Link>
               </small>
             </form>
           </div>
